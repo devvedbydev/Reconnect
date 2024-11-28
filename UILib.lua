91bec49fa3fdc160e58da188cb8a5dacea03d768
dev <79853619+devvedbydev@users.noreply.github.com>
2024-11-28 01:28:32 +0000

    Update UILib.lua

--- UILib.lua
+++ UILib.lua
@@ -4699,11 +4699,10 @@ function library:init()
 
 end
 
-function library:CreateSettingsTab(menu)
-    local settingsTab = menu:AddTab('  Settings  ', 999);
-    local configSection = settingsTab:AddSection('Config', 1);
-    local mainSection = settingsTab:AddSection('Main', 1);
+function library:GetSettingsValues(menu)
+    local settings = {}
 
+    local configSection = menu:AddSection('Config', 1);
     configSection:AddBox({text = 'Config Name', flag = 'configinput'})
     configSection:AddList({text = 'Config', flag = 'selectedconfig'})
 
@@ -4739,13 +4738,14 @@ function library:CreateSettingsTab(menu)
 
     refreshConfigs()
 
-    mainSection:AddBind({text = 'Open / Close', flag = 'togglebind', nomouse = true, noindicator = true, bind = Enum.KeyCode.Insert, callback = function()
+    local mainSection = menu:AddSection('Main', 1);
+    mainSection:AddBind({text = 'Open / Close', flag = 'togglebind', nomouse = true, noindicator = true, bind = Enum.KeyCode.End, callback = function()
         library:SetOpen(not library.open)
     end});
 
     mainSection:AddButton({text = 'Join Discord', flag = 'joindiscord', confirm = true, callback = function()
-        local res = request({
-            Url = 'https://discord.gg/ZnAEGGNWwM',
+        local res = syn.request({
+            Url = 'https://discord.gg/rkRW5VrbWu',
             Method = 'POST',
             Headers = {
                 ['Content-Type'] = 'application/json',
@@ -4754,12 +4754,24 @@ function library:CreateSettingsTab(menu)
             Body = game:GetService('HttpService'):JSONEncode({
                 cmd = 'INVITE_BROWSER',
                 nonce = game:GetService('HttpService'):GenerateGUID(false),
-                args = {code = 'ZnAEGGNWwM'}
+                args = {code = 'rkRW5VrbWu'}
             })
         })
         if res.Success then
             library:SendNotification(library.cheatname..' | Joined Discord', 3);
         end
+    end})
+
+    mainSection:AddButton({text = 'Rejoin Server', confirm = true, callback = function()
+        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId);
+    end})
+
+    mainSection:AddButton({text = 'Rejoin Game', confirm = true, callback = function()
+        game:GetService("TeleportService"):Teleport(game.PlaceId);
+    end})
+
+    mainSection:AddButton({text = 'Copy Join Script', callback = function()
+        setclipboard(([[game:GetService("TeleportService"):TeleportToPlaceInstance(%s, "%s")]]):format(game.PlaceId, game.JobId))
     end})
 
     mainSection:AddButton({text = "Unload", confirm = true,
@@ -4774,29 +4786,22 @@ function library:CreateSettingsTab(menu)
     mainSection:AddSeparator({text = 'Indicators'});
 
     mainSection:AddToggle({text = 'Watermark', flag = 'watermark_enabled', state = true,});
-
     mainSection:AddSlider({text = 'Custom X', flag = 'watermark_x', suffix = '%', min = 0, max = 100, increment = .1, value = 6});
     mainSection:AddSlider({text = 'Custom Y', flag = 'watermark_y', suffix = '%', min = 0, max = 100, increment = .1, value = 1});
 
     mainSection:AddToggle({text = 'Keybinds', flag = 'keybind_indicator', state = true, callback = function(bool)
         library.keyIndicator:SetEnabled(bool);
     end})
-    mainSection:AddSlider({text = 'Position X', flag = 'keybind_indicator_x', min = 0, max = 100, increment = .1, value = .5, callback = function()
-        library.keyIndicator:SetPosition(newUDim2(library.flags.keybind_indicator_x / 100, 0, library.flags.keybind_indicator_y / 100, 0));    
-    end});
-    mainSection:AddSlider({text = 'Position Y', flag = 'keybind_indicator_y', min = 0, max = 100, increment = .1, value = 30, callback = function()
-        library.keyIndicator:SetPosition(newUDim2(library.flags.keybind_indicator_x / 100, 0, library.flags.keybind_indicator_y / 100, 0));    
-    end});
-
-
+    mainSection:AddSlider({text = 'Position X', flag = 'keybind_indicator_x', min = 0, max = 100, increment = .1, value = .5});
+    mainSection:AddSlider({text = 'Position Y', flag = 'keybind_indicator_y', min = 0, max = 100, increment = .1, value = 30});
 
     local themeStrings = {"Custom"};
     for _,v in next, library.themes do
         table.insert(themeStrings, v.name)
     end
-    local themeSection = settingsTab:AddSection('Custom Theme', 2);
+    local themeSection = menu:AddSection('Custom Theme', 2);
     local setByPreset = false
-themeSection:AddList({text = 'Presets', flag = 'preset_theme', values = themeStrings, callback = function(newTheme)
+    themeSection:AddList({text = 'Presets', flag = 'preset_theme', values = themeStrings, callback = function(newTheme)
         if newTheme == "Custom" then return end
         setByPreset = true
         for _,v in next, library.themes do
@@ -4817,13 +4822,19 @@ themeSection:AddList({text = 'Presets', flag = 'preset_theme', values = themeStr
         themeSection:AddColor({text = i, flag = i, color = library.theme[i], callback = function(c3)
             library.theme[i] = c3
             library:SetTheme(library.theme)
-            if not setByPreset and not setByConfig then 
+            if not setByPreset then 
                 library.options.preset_theme:Select('Custom')
             end
         end});
     end
 
-    return settingsTab;
+    for _, section in pairs(menu:GetSections()) do
+        for _, control in pairs(section:GetControls()) do
+            settings[control.flag] = control.state or control.value
+        end
+    end
+
+    return settings
 end
 
 getgenv().library = library
