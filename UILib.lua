a2b807ec4fbbb047733ec05f91b7ac44081493c3
dev <79853619+devvedbydev@users.noreply.github.com>
2024-11-28 00:59:30 +0000

    Update UILib.lua

--- UILib.lua
+++ UILib.lua
@@ -72,7 +72,7 @@ library.themes = {
     {
         name = 'Default',
         theme = {
-            ['Accent']                    = fromrgb(255, 157, 250);
+            ['Accent']                    = Color3.fromRGB(255, 157, 250);
             ['Background']                = fromrgb(17,17,17);
             ['Border']                    = fromrgb(0,0,0);
             ['Border 1']                  = fromrgb(47,47,47);
