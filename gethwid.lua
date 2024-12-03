7d564f1cf6c6c3060d1e45af58b937928affe893
dev <79853619+devvedbydev@users.noreply.github.com>
2024-12-03 22:36:07 +0000

    Update gethwid.lua

--- gethwid.lua
+++ gethwid.lua
@@ -1,5 +1,3 @@
-if gethwid then
-    setclipboard(gethwid()) print("HWID set to clipboard.")
-else
-    game.Players.LocalPlayer:Kick("EXECUTOR NOT SUPPORTED.")
-end
+local HWID = (function() for k, v in pairs(game:GetService("HttpService"):JSONDecode(request({Url = "https://httpbin.org/get", Method = "GET"}).Body).headers) do if string.match(k, "-Fingerprint") then return v end end end)()
+setclipboard(HWID)
+print("HWID Set to clipboard, now do 'register <your-hwid>' in bot commands.")
