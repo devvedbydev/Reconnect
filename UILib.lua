a1e9f7f736a9593416e51b7c25bd9d7e670e566d
dev <79853619+devvedbydev@users.noreply.github.com>
2024-12-08 12:59:16 +0000

    Update UILib.lua

--- UILib.lua
+++ UILib.lua
@@ -13,7 +13,6 @@ local isStudio = RunService:IsStudio()
 local LocalPlayer = Players.LocalPlayer
 
 local windowState
-local acrylicBlur
 local hasGlobalSetting
 
 local tabs = {}
@@ -46,11 +45,6 @@ end
 --// Library Functions
 function MacLib:Window(Settings)
 	local WindowFunctions = {Settings = Settings}
-	if Settings.AcrylicBlur ~= nil then
-		acrylicBlur = Settings.AcrylicBlur
-	else
-		acrylicBlur = true
-	end
 
 	local macLib = Instance.new("ScreenGui")
 	macLib.Name = "MacLib"
@@ -91,7 +85,7 @@ function MacLib:Window(Settings)
 	base.Name = "Base"
 	base.AnchorPoint = Vector2.new(0.5, 0.5)
 	base.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-	base.BackgroundTransparency = Settings.AcrylicBlur and 0.05 or 0
+	base.BackgroundTransparency = 1
 	base.BorderColor3 = Color3.fromRGB(0, 0, 0)
 	base.BorderSizePixel = 0
 	base.Position = UDim2.fromScale(0.5, 0.5)
@@ -1119,7 +1113,7 @@ function MacLib:Window(Settings)
 	end
 
 	local function UpdateOrientation(fetchProps)
-		if not IsVisible(frame) or not acrylicBlur or unloaded then
+		if not IsVisible(frame) or unloaded then
 			for _, pt in pairs(parts) do
 				pt.Parent = nil
 				DepthOfField.Enabled = false
@@ -5302,15 +5296,6 @@ function MacLib:Window(Settings)
 		MenuKeybind = Keycode
 	end
 
-	function WindowFunctions:SetAcrylicBlurState(State)
-		acrylicBlur = State
-		base.BackgroundTransparency = State and 0.05 or 0
-	end
-
-	function WindowFunctions:GetAcrylicBlurState()
-		return acrylicBlur
-	end
-
 	local function _SetUserInfoState(State)
 		if State then
 			headshot.Image = (isReady and headshotImage) or "rbxassetid://0"
@@ -5607,15 +5592,14 @@ function MacLib:Demo()
 		DisabledWindowControls = {},
 		ShowUserInfo = true,
 		Keybind = Enum.KeyCode.RightControl,
-		AcrylicBlur = true,
+		AcrylicBlur = false,
 	})
 
 	local globalSettings = {
 		UIBlurToggle = Window:GlobalSetting({
 			Name = "UI Blur",
-			Default = Window:GetAcrylicBlurState(),
+			Default = false,
 			Callback = function(bool)
-				Window:SetAcrylicBlurState(bool)
 				Window:Notify({
 					Title = Window.Settings.Title,
 					Description = (bool and "Enabled" or "Disabled") .. " UI Blur",
