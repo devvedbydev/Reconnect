3364ffe152a6bc92fde6af46afaa2ec92f5977fe
dev <79853619+devvedbydev@users.noreply.github.com>
2025-01-03 17:56:02 +0000

    Update esplib.lua

--- esplib.lua
+++ esplib.lua
@@ -202,7 +202,7 @@ get("player").update = function(self, character, data)
 			drawings.boxOutline.ZIndex = drawings.box.ZIndex - 1
 			drawings.boxFilled.ZIndex = drawings.boxOutline.ZIndex - 1
 
-			drawings.name.Text = `{player.Name}`
+			drawings.name.Text = {player.Name}
 			drawings.name.Size = 13
 			drawings.name.Font = 2
 			drawings.name.Position = Vector2.new(x, (yPostion - drawings.name.TextBounds.Y) - 2)
@@ -218,7 +218,7 @@ get("player").update = function(self, character, data)
 			drawings.healthOutline.To = Vector2.new(xPosition - 5, yPostion + height)
 			drawings.health.From = Vector2.new(xPosition - 5, (yPostion + height) - 1)
 			drawings.health.To = Vector2.new(xPosition - 5, ((drawings.health.From.Y - ((height / 100) * healthPercent))) + 2)
-			drawings.healthText.Text = `HP {math.floor(humanoid.Health)}`
+			drawings.healthText.Text = HP {math.floor(humanoid.Health)}
 			drawings.healthText.Size = 13
 			drawings.healthText.Font = 2
 			drawings.healthText.Position = Vector2.new(drawings.health.To.X - (drawings.healthText.TextBounds.X + 3), (drawings.health.To.Y - (2 / scale)))
@@ -232,7 +232,7 @@ get("player").update = function(self, character, data)
 
 			drawings.healthOutline.ZIndex = drawings.health.ZIndex - 1
 
-			drawings.distance.Text = `{math.floor(data.distance)}`
+			drawings.distance.Text = {math.floor(data.distance)}
 			drawings.distance.Size = 13
 			drawings.distance.Font = 2
 			drawings.distance.Position = Vector2.new(x, (yPostion + height) + (drawings.distance.TextBounds.Y * 0.25))
@@ -240,10 +240,10 @@ get("player").update = function(self, character, data)
 			drawings.distance.Outline = visuals.distance.outline.enabled
 			drawings.distance.OutlineColor = visuals.distance.outline.color
 			
-			drawings.weapon.Text = `{weapon}`
+			drawings.weapon.Text = {weapon}
 			drawings.weapon.Size = 13
 			drawings.weapon.Font = 2
-			drawings.weapon.Position = visuals.weapon.enabled and Vector2.new(drawings.distance.Position.x, drawings.distance.Position.Y + drawings.distance.TextBounds.Y + 5) or drawings.distance.Position
+			drawings.weapon.Position = visuals.distance.enabled and Vector2.new(drawings.distance.Position.x, drawings.distance.Position.Y + (drawings.weapon.TextBounds.Y * 0.75)) or drawings.distance.Position
 			drawings.weapon.Color = color(visuals.weapon.color)
 			drawings.weapon.Outline = visuals.weapon.outline.enabled
 			drawings.weapon.OutlineColor = visuals.weapon.outline.color
