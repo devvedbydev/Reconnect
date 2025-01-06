f8efbb65caeea721754e3cea0589f15f1a26964d
dev <79853619+devvedbydev@users.noreply.github.com>
2025-01-06 18:15:50 +0000

    Create ViewTracer.lua

---
+++ ViewTracer.lua
@@ -0,0 +1,72 @@
+local Settings = {
+    Color = Color3.fromRGB(255, 255, 255),
+    Thickness = 1,
+    Transparency = 1,
+    AutoThickness = true,
+    Length = 5,
+    Smoothness = 0.2
+}
+
+local toggle = true
+local player = game:GetService("Players").LocalPlayer
+local camera = game:GetService("Workspace").CurrentCamera
+
+local function ESP(plr)
+    local line = Drawing.new("Line")
+    line.Visible = false
+    line.From = Vector2.new(0, 0)
+    line.To = Vector2.new(0, 0)
+    line.Color = Settings.Color
+    line.Thickness = Settings.Thickness
+    line.Transparency = Settings.Transparency
+
+    local function Updater()
+        local connection
+        connection = game:GetService("RunService").RenderStepped:Connect(function()
+            if toggle and plr.Character ~= nil and plr.Character:FindFirstChild("Humanoid") ~= nil and plr.Character:FindFirstChild("HumanoidRootPart") ~= nil and plr.Character.Humanoid.Health > 0 and plr.Character:FindFirstChild("Head") ~= nil then
+                local headpos, OnScreen = camera:WorldToViewportPoint(plr.Character.Head.Position)
+                if OnScreen then
+                    local offsetCFrame = CFrame.new(0, 0, -Settings.Length)
+                    local check = false
+                    line.From = Vector2.new(headpos.X, headpos.Y)
+                    if Settings.AutoThickness then
+                        local distance = (player.Character.HumanoidRootPart.Position - plr.Character.HumanoidRootPart.Position).magnitude
+                        local value = math.clamp(1/distance*100, 0.1, 3)
+                        line.Thickness = value
+                    end
+                    repeat
+                        local dir = plr.Character.Head.CFrame:ToWorldSpace(offsetCFrame)
+                        offsetCFrame = offsetCFrame * CFrame.new(0, 0, Settings.Smoothness)
+                        local dirpos, vis = camera:WorldToViewportPoint(Vector3.new(dir.X, dir.Y, dir.Z))
+                        if vis then
+                            check = true
+                            line.To = Vector2.new(dirpos.X, dirpos.Y)
+                            line.Visible = true
+                            offsetCFrame = CFrame.new(0, 0, -Settings.Length)
+                        end
+                    until check == true
+                else 
+                    line.Visible = false
+                end
+            else 
+                line.Visible = false
+                if game.Players:FindFirstChild(plr.Name) == nil then
+                    connection:Disconnect()
+                end
+            end
+        end)
+    end
+    coroutine.wrap(Updater)()
+end
+
+for i, v in pairs(game:GetService("Players"):GetPlayers()) do
+    if v.Name ~= player.Name then
+        coroutine.wrap(ESP)(v)
+    end
+end
+
+game.Players.PlayerAdded:Connect(function(newplr)
+    if newplr.Name ~= player.Name then
+        coroutine.wrap(ESP)(newplr)
+    end
+end)
