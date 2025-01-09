dc4625de6276746510da56590828bc165a3370e2
dev <79853619+devvedbydev@users.noreply.github.com>
2025-01-09 16:34:03 +0000

    Update BanDetection.lua

--- BanDetection.lua
+++ BanDetection.lua
@@ -1,37 +1,20 @@
-local StarterGui = game:GetService("StarterGui")
 local LogService = game:GetService("LogService")
 
 hookfunction(print, function(message)
-    StarterGui:SetCore("SendNotification", {
-        Title = "Print Detected",
-        Text = message,
-        Duration = 5
-    })
+    writefile("log.txt", "Print Detected: " .. message .. "\n")
     return message
 end)
 
 hookfunction(warn, function(message)
-    StarterGui:SetCore("SendNotification", {
-        Title = "Warning Detected",
-        Text = message,
-        Duration = 5
-    })
+    writefile("log.txt", "Warning Detected: " .. message .. "\n")
     return message
 end)
 
 hookfunction(error, function(message)
-    StarterGui:SetCore("SendNotification", {
-        Title = "Error Detected",
-        Text = message,
-        Duration = 5
-    })
+    writefile("log.txt", "Error Detected: " .. message .. "\n")
     return message
 end)
 
 LogService.MessageOut:Connect(function(message, messageType)
-    StarterGui:SetCore("SendNotification", {
-        Title = messageType.Name,
-        Text = message,
-        Duration = 5
-    })
+    writefile("log.txt", messageType.Name .. ": " .. message .. "\n")
 end)
